0fac958b50585ac98f77b1bf185c71385befe321
marietuco11 <marietuco11@gmail.com>
2026-03-21 12:18:35 +0100

    Pantallas de explorar y mapa

--- EventConnect/src/app/features/map/map.component.ts
+++ EventConnect/src/app/features/map/map.component.ts
@@ -1,9 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, PLATFORM_ID, AfterViewInit } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { EventService } from '../../core/services/event.service';
+import { HeaderComponent } from '../../layout/components/header/header';
 
 @Component({
   selector: 'app-map',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink, HeaderComponent],
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss',
 })
-export class MapComponent {}
+export class MapComponent implements OnInit, AfterViewInit {
+  events: any[] = [];
+  selectedEvent: any = null;
+  private platformId = inject(PLATFORM_ID);
+  private eventService = inject(EventService);
+  private map: any;
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.eventService.getEvents(1, 100).subscribe({
+        next: (res) => {
+          this.events = res.data.filter((e: any) => e.latitude && e.longitude);
+          if (this.map) this.addMarkers();
+        }
+      });
+    }
+  }
+
+  async ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      const L = await import('leaflet');
+
+      this.map = L.map('map', {
+        center: [41.6488, -0.8891],
+        zoom: 13
+      });
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap contributors'
+      }).addTo(this.map);
+
+      if (this.events.length) this.addMarkers();
+    }
+  }
+
+  async addMarkers() {
+    const L = await import('leaflet');
+    this.events.forEach(event => {
+      const marker = L.marker([event.latitude, event.longitude])
+        .addTo(this.map)
+        .bindPopup(`<strong>${event.title}</strong><br>${event.locationName}`);
+
+      marker.on('click', () => {
+        this.selectedEvent = event;
+      });
+    });
+  }
+}
